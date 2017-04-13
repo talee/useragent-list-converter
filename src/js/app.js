58f276ec38c132b88c03881676bc28ab02f0538c
Thomas Lee <tleehoc@gmail.com>
2017-04-13 02:18:01 -0700

    Working version

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,10 @@
+import setupParser from 'uap-ref-impl'
+import regexesStr from './regexes'
+import yaml from 'yamlparser'
+
+const regexes = yaml.eval(regexesStr)
+const parser = setupParser(regexes)
+
 if (document.readyState == 'loading') {
   document.addEventListener('DOMContentLoaded', start)
 } else {
@@ -5,5 +12,19 @@ if (document.readyState == 'loading') {
 }
 
 function start() {
-  console.log('Hello world!')
+  const uaRawList = document.getElementById('user-agents-raw')
+  const uaConvertedList = document.getElementById('user-agents-converted')
+  const convertButton = document.getElementById('convert')
+
+  convertButton.addEventListener('click', convert)
+
+  function convert() {
+    let uaConvertedStr = ''
+    const uaStrs = uaRawList.value.trim().split('\n')
+    for (const uaStr of uaStrs) {
+      const agent = parser.parse(uaStr)
+      uaConvertedStr += `${agent.ua.family} ${agent.ua.major}\n`
+    }
+    uaConvertedList.value = uaConvertedStr
+  }
 }
